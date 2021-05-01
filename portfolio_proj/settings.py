cd00a5bc97d2ec487e8b56ca5553edd946544626
sanket758 <gadgesanket75@gmail.com>
2021-05-02 00:54:18 +0530

    fix data

--- portfolio_proj/settings.py
+++ portfolio_proj/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG_FLAG']
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','sanketgadge.herokuapp.com']
 
